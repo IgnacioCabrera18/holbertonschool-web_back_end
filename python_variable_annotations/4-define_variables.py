a748e484c20609db82a9390ff49c44a2d52418eb
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-11-29 17:07:54 +0000

    done

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Variable definition module.
+
+This module defines several annotated variables with fixed values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
