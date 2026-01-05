7aa54cb0c7f4734a8693d9702c431d6e9cbf30a1
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-01-04 22:23:27 -0300

    Done

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """ Log stats """
+
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+client = MongoClient('mongodb://127.0.0.1:27017')
+collection = client.logs.nginx
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+count_logs = collection.count_documents({})
+print("{} logs".format(count_logs))
 
-    count_logs = collection.count_documents({})
-    print("{} logs".format(count_logs))
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    count_method = collection.count_documents({"method": method})
+    print("    method {}: {}".format(method, count_method))
 
-    print("Methods:")
-    for method in methods:
-        count_method = collection.count_documents({"method": method})
-        print("    method {}: {}".format(method, count_method))
-
-    check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print("{} status check".format(check))
+check = collection.count_documents({"method": "GET", "path": "/status"})
+print("{} status check".format(check))
